a23b3e62aba1832b41532f4b070cc3fe4bc8955f
Sagar Ajudiya <>
2025-06-11 09:54:58 +0530

    Initial Setup

---
+++ CountryList/App/CountryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryListApp.swift
+//  CountryList
+//
+//  Created by MACM06 on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
