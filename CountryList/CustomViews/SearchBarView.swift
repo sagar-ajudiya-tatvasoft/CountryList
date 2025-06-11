263e8380752085e1fed62a7363a174a6f81aadcf
Sagar Ajudiya <>
2025-06-11 14:19:43 +0530

    ADD- SearchView

---
+++ CountryList/CustomViews/SearchBarView.swift
@@ -0,0 +1,42 @@
+//
+//  SearchBarView.swift
+//  CountryList
+//
+//  Created by MACM06 on 11/06/25.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    // MARK: - Variable
+    let placeholder: String
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            // Left Icon
+            Image(.icSearch)
+            // TextField
+            TextField(
+                placeholder,
+                text: $searchText,
+                prompt: Text(placeholder)
+                    .font(.system(size: 14, weight: .medium))
+                    .foregroundStyle(.appDarkGrey)
+            )
+            .tint(.appDarkGrey)
+            .autocorrectionDisabled(true)
+            .textInputAutocapitalization(.never)
+            .submitLabel(.done)
+        }
+        .padding(15)
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(.appBlack, lineWidth: 0.5)
+        }
+    }
+}
+
+#Preview {
+    SearchBarView(placeholder: "", searchText: .constant(""))
+}
